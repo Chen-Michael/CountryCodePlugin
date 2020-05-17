2953ceecd5e4c3ff61bf8ad11edda30172e8acb8
Mike <mikechen@core-tech.tw>
2020-05-17 23:06:17 +0800

    change function

--- CountryCodePlugin/CountryCodeTableViewController.swift
+++ CountryCodePlugin/CountryCodeTableViewController.swift
@@ -22,10 +22,10 @@ public class CountryCodeTableViewController: UITableViewController {
     }
     
     private func loadData() {
-        NSLog(Bundle(for: type(of: self)).path(forResource: "CountryCodeList\(language)", ofType: "plist") ?? "nil")
-        NSLog(Bundle(for: type(of: self)).path(forResource: "CountryCodeList\(language)", ofType: "plist", inDirectory: "Resources") ?? "nil")
-        NSLog(Bundle(for: type(of: self)).url(forResource: "CountryCodeList\(language)", withExtension: "plist")?.absoluteString ?? "nil")
-        NSLog(Bundle(for: type(of: self)).url(forResource: "CountryCodeList\(language)", withExtension: "plist", subdirectory: "Resources")?.absoluteString ?? "nil")
+        print(Bundle(for: type(of: self)).path(forResource: "CountryCodeList\(language)", ofType: "plist") ?? "nil")
+        print(Bundle(for: type(of: self)).path(forResource: "CountryCodeList\(language)", ofType: "plist", inDirectory: "Resources") ?? "nil")
+        print(Bundle(for: type(of: self)).url(forResource: "CountryCodeList\(language)", withExtension: "plist")?.absoluteString ?? "nil")
+        print(Bundle(for: type(of: self)).url(forResource: "CountryCodeList\(language)", withExtension: "plist", subdirectory: "Resources")?.absoluteString ?? "nil")
         if let URL = Bundle(for: type(of: self)).path(forResource: "CountryCodeList\(language)", ofType: "plist"), let list = NSArray(contentsOfFile: URL) as? [NSDictionary] {
             for item in list {
                 code.append(item.allKeys[0] as! String)
